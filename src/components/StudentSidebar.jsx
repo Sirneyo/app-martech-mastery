d1185038a812df4f9d21b26a5043856ae09356de
Ola Daranijo <ola@oadsolutions.com>
2026-01-13 01:48:42 +0000

    File changes

--- src/components/StudentSidebar.jsx
+++ src/components/StudentSidebar.jsx
@@ -41,7 +41,7 @@ export default function StudentSidebar({ currentPageName, onNavigate }) {
           <img 
             src="https://storage.googleapis.com/msgsndr/DVqsiywKVWkfZ4I0mXQ1/media/693348610439b8283bf88818.svg" 
             alt="MarTech Mastery" 
-            className="w-full h-auto"
+            className="w-[90%] h-auto"
           />
         </Link>
       </div>
